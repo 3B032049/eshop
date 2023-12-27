c1f024e61f8cceabc8926a0c341ccc5b45f1c6a1
鄭哲丞 <a0968518713@gmail.com>
2023-12-27 11:04:26 +0800

    安裝laravel/breeze

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,20 +17,15 @@ use App\Http\Controllers\ProductController;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::resource('products', ProductController::class);
-/*
-products.index:     GET         /                       ProductController@index
-                    列出所有產品
-products.show:      GET|HEAD    products/{product}      ProductController@show
-                    檢視某一產品
-products.create:    GET         products                ProductController@create
-                    產生產品新增的表單
-products.store:     POST        products                ProductController@store
-                    儲存新增的產品
-products.edit:      GET|HEAD    products/{product}/edit ProductController@edit
-                    產生某一產品修改的表單
-products.update:    PUT|PATCH   products/{product}      ProductController@update
-                    更新某一產品
-products.destroy:   DELETE      products/{product}      ProductController@destroy
-                    刪除某一產品
-*/
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
