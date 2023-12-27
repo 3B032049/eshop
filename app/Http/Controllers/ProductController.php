21f1cc0c5f464508fb295844c8afb62eddb74dab
鄭哲丞 <a0968518713@gmail.com>
2023-12-27 12:33:21 +0800

    讓分類商品頁面的商品可點選，以進入products.show路由檢視商品內容

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,8 +38,11 @@ class ProductController extends Controller
      */
     public function show(Product $product)
     {
-        return 'show';
-
+    //dd($product);
+        $data = [
+            'product' => $product,
+        ];
+        return view('products.show', $data);
     }
 
     /**
