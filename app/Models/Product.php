4604f2ddc9499dcaa92f82f43e29c26f34f3fcde
鄭哲丞 <a0968518713@gmail.com>
2023-12-27 11:00:24 +0800

    建立各個model之間的關係

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -11,4 +11,18 @@ class Product extends Model
     public $fillable = [
         'name',
     ];
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function cart_items()
+    {
+        return $this->hasMany(CartItem::class);
+    }
+
+    public function order_items()
+    {
+        return $this->hasMany(OrderItem::class);
+    }
 }
